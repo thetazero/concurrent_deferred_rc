b5657917a61875baf1e66d74b4546005dbe6ce57
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-02-17 20:54:38 -0500

    build asan tests as well

--- build_and_run_test.sh
+++ build_and_run_test.sh
@@ -2,7 +2,8 @@
 
 mkdir -p build
 cd build
-cmake -DCDRC_TEST=On -DBUILD_ASAN_TESTS=On .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure
+FLAGS="-DBUILD_ASAN_TESTS=On "
+cmake -DCDRC_TEST=On $FLAGS .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure
 # cmake -DCDRC_TEST=On -DBUILD_ASAN_TESTS=On .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure -R TestWeakPtrLeak
 # cmake -DCDRC_TEST=On -DBUILD_ASAN_TESTS=On .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure -R TestBug
 # cmake -DCDRC_TEST=On -DBUILD_ASAN_TESTS=On .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure -R TestSticky

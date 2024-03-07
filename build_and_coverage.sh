0a2610ee374d18c54c78511b7f390a8c1317cdc4
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-03-07 11:51:05 -0500

    add some pointer tests (#10)

--- build_and_coverage.sh
+++ build_and_coverage.sh
@@ -2,5 +2,4 @@
 
 mkdir -p build
 cd build
-FLAGS="-DCOVERAGE=On -DBUILD_ASAN_TESTS=Off"
-cmake -DCDRC_TEST=On $FLAGS .. && cmake --build . && ctest -C Debug --no-tests=error --output-on-failure
+cmake -DCDRC_TEST=On -DCOVERAGE=On -DBUILD_ASAN_TESTS=Off .. && cmake --build . && make coverage_report
